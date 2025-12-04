68d843cd8c19e93264d966e341fc73b25c2f1194
Shiver3d <danielm8lopes@gmail.com>
2025-12-04 19:47:06 -0300

    não tive avanços significativos hoje, apesar do site já estar funcionando realmente penso em features que gostaria de experimentar, mas talvez deixe isso para semana... No momento, criei apenas o .vue da aba favoritos. Será feita a página perfil e a implementação das duas rotas no próximo commit

---
+++ routes/Favoritos.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>⭐ Meus Favoritos</h1>
+    <p>Aqui você verá a lista de itens favoritos do usuário.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss"></style>
